26988d03c12010c497842b1ca50b56c4e51829d0
Taylor Smith <tas0018@auburn.edu>
2021-08-16 15:01:47 -0500

    Finished tab functionallity

---
+++ main.js
@@ -0,0 +1,18 @@
+const tabs = document.querySelectorAll("[data-tab-target]");
+const tabContents = document.querySelectorAll("[data-tab-target]");
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    const target = document.querySelector(tab.dataset.tabTarget);
+    tabContents.forEach((tabContent) => {
+      const truck = document.getElementById("food-truck-menu");
+      const night = document.getElementById("late-night-menu");
+      const full = document.getElementById("full-menu");
+      truck.classList.remove("active");
+      night.classList.remove("active");
+      full.classList.remove("active");
+    });
+
+    target.classList.add("active");
+  });
+});
